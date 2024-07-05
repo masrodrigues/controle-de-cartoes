4dd0e93cbd9aaba3098b3db87e34a5dc78bde21b
Marco Rodrigues <84645887+masrodrigues@users.noreply.github.com>
2024-07-05 08:22:04 -0300

    feat: ajustes modal

--- gestao_cartoes/views.py
+++ gestao_cartoes/views.py
@@ -1,32 +1,29 @@
+import logging
+from datetime import date, timedelta
+
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db import transaction
+from django.db.models import Sum
+from django.http import JsonResponse
 from django.shortcuts import render, redirect, get_object_or_404
-from django.urls import reverse, reverse_lazy
+from django.urls import reverse_lazy
+from django.utils import timezone
 from django.views.generic import ListView, DetailView, TemplateView, CreateView, UpdateView
 from django.views.generic.edit import DeleteView
-from django.utils import timezone
-from django.db import transaction
-from django.db.models import Sum, Q
-from datetime import datetime, date, timedelta
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-from .models import Cartao, Gasto
-from .forms import GastoForm, CartaoForm, CategoriaForm, ConfirmDeleteForm
 
-import logging
+from .forms import GastoForm, CartaoForm, CategoriaForm, ConfirmDeleteForm
+from .models import Cartao, Gasto, Categoria
 
 # Configura o logger para o Django
 logger = logging.getLogger(__name__)
-from django.contrib.auth.mixins import LoginRequiredMixin
 
 class ListaCartoesView(LoginRequiredMixin, ListView):
     model = Cartao
     template_name = 'lista_cartoes.html'
     context_object_name = 'cartoes'
-
-
-# Detalhes do Cartão
-from datetime import date, timedelta
-from django.views.generic.detail import DetailView
-from .models import Cartao, Gasto
 
 class DetalhesCartaoView(DetailView):
     model = Cartao
@@ -49,11 +46,7 @@ class DetalhesCartaoView(DetailView):
             ano_vencimento = hoje.year
 
         proximo_vencimento = date(ano_vencimento, mes_vencimento, vencimento_dia)
-
-        # Ajusta a data de fechamento para ser 10 dias antes do próximo vencimento
         data_fim = proximo_vencimento - timedelta(days=10)
-
-        # Calcula a data de início como 30 dias antes da data de fim
         data_inicio = data_fim - timedelta(days=30)
 
         gastos = Gasto.objects.filter(cartao=cartao, data__gte=data_inicio, data__lt=data_fim)
@@ -64,7 +57,6 @@ class DetalhesCartaoView(DetailView):
         context['gastos'] = gastos
         return context
 
-# Adicionar Gasto
 class AdicionarGastoView(CreateView):
     model = Gasto
     form_class = GastoForm
@@ -75,7 +67,7 @@ class AdicionarGastoView(CreateView):
         with transaction.atomic():
             self.object = form.save(commit=False)
             parcelas = form.cleaned_data.get('parcelas', 1)
-            self.object.save()  # Salva o primeiro gasto
+            self.object.save()
             for i in range(1, parcelas):
                 gasto_parcelado = Gasto(
                     cartao=self.object.cartao,
@@ -88,18 +80,11 @@ class AdicionarGastoView(CreateView):
                 gasto_parcelado.save()
         return super().form_valid(form)
 
-# Editar Gasto
 class EditarGastoView(UpdateView):
     model = Gasto
     form_class = GastoForm
     template_name = 'editar_gasto.html'
     success_url = reverse_lazy('lista_cartoes')
-
-from django.views.generic import TemplateView
-from django.utils import timezone
-from django.db.models import Sum
-from datetime import date, timedelta
-from .models import Gasto, Cartao
 
 class RelatorioGastosView(TemplateView):
     template_name = 'relatorio_gastos.html'
@@ -117,22 +102,17 @@ class RelatorioGastosView(TemplateView):
         context['nome_mes_selecionado'] = self.mapear_numero_para_mes(mes_atual)
         context['meses'] = [(i, self.mapear_numero_para_mes(i)) for i in range(1, 13)]
 
-        # Filtrar gastos por mês usando o período de fechamento
         relatorio = []
         total_gastos_geral = 0
         cartoes = Cartao.objects.all()
 
         for cartao in cartoes:
-            # Definindo o próximo vencimento para o mês atual selecionado
             mes_vencimento = mes_atual if mes_atual >= date.today().month else mes_atual + 1
             ano_vencimento = ano_atual if mes_vencimento >= date.today().month else ano_atual + 1
             proximo_vencimento = date(ano_vencimento, mes_vencimento, cartao.vencimento)
-
-            # Ajusta a data de fechamento para ser 10 dias antes do próximo vencimento
             data_fim = proximo_vencimento - timedelta(days=10)
             data_inicio = data_fim - timedelta(days=30)
 
-            # Filtra os gastos do cartão usando o período de fechamento
             gastos = Gasto.objects.filter(cartao=cartao, data__gte=data_inicio, data__lt=data_fim)
             total_gastos = gastos.aggregate(Sum('valor'))['valor__sum'] or 0
 
@@ -142,7 +122,6 @@ class RelatorioGastosView(TemplateView):
 
         context['relatorio'] = relatorio
         context['total_gastos_geral'] = total_gastos_geral
-
         return context
 
     def mapear_numero_para_mes(self, numero_mes):
@@ -153,11 +132,6 @@ class RelatorioGastosView(TemplateView):
         }
         return meses.get(numero_mes, "Mês Inválido")
 
-
-
-
-
-# Adicionar Cartão
 class AdicionarCartaoView(CreateView):
     model = Cartao
     form_class = CartaoForm
@@ -165,13 +139,11 @@ class AdicionarCartaoView(CreateView):
     success_url = reverse_lazy('lista_cartoes')
 
     def get_form_kwargs(self):
-        """Este método é sobrescrito para incluir request.FILES no formulário."""
         kwargs = super().get_form_kwargs()
         if self.request.method in ('POST', 'PUT'):
             kwargs['files'] = self.request.FILES
         return kwargs
 
-# Editar Cartão
 def editar_cartao(request, cartao_id):
     cartao = get_object_or_404(Cartao, id=cartao_id)
     if request.method == 'POST':
@@ -183,43 +155,27 @@ def editar_cartao(request, cartao_id):
         form = CartaoForm(instance=cartao)
     return render(request, 'editar_cartao.html', {'form': form})
 
-# Excluir Cartão
 def excluir_cartao_view(request, pk):
     cartao = get_object_or_404(Cartao, pk=pk)
     if request.method == 'POST':
         form = ConfirmDeleteForm(request.POST)
         if form.is_valid() and form.cleaned_data['confirm']:
             cartao.delete()
-            return redirect(reverse('lista_cartoes'))
+            return redirect('lista_cartoes')
     else:
         form = ConfirmDeleteForm()
     return render(request, 'confirmar_exclusao.html', {'form': form, 'cartao': cartao})
 
-# Excluir Gasto
 def excluir_gasto(request, gasto_id):
     gasto = get_object_or_404(Gasto, id=gasto_id)
     cartao_id = gasto.cartao.id
     gasto.delete()
     return redirect('detalhes_cartao', pk=cartao_id)
 
-# Gastos por Categoria
 def gastos_por_categoria(request, categoria_id):
     gastos = Gasto.objects.filter(categoria__id=categoria_id)
     return render(request, 'gastos_por_categoria.html', {'gastos': gastos})
 
-# Cadastro de Categoria
-from django.shortcuts import render, redirect, get_object_or_404
-from django.urls import reverse
-from django.views.generic import ListView, DetailView, TemplateView, CreateView, UpdateView
-from django.views.generic.edit import DeleteView
-from django.utils import timezone
-from django.db import transaction
-from django.db.models import Sum, Q
-from datetime import datetime, date, timedelta
-from .models import Cartao, Gasto, Categoria
-from .forms import GastoForm, CartaoForm, CategoriaForm, ConfirmDeleteForm
-
-# Cadastro de Categoria
 def cadastro_categoria(request):
     if request.method == 'POST':
         form = CategoriaForm(request.POST)
@@ -232,7 +188,6 @@ def cadastro_categoria(request):
     categorias = Categoria.objects.all()
     return render(request, 'cadastro_categoria.html', {'form': form, 'categorias': categorias})
 
-# Editar Categoria
 def editar_categoria(request, categoria_id):
     categoria = get_object_or_404(Categoria, id=categoria_id)
     if request.method == 'POST':
@@ -244,17 +199,12 @@ def editar_categoria(request, categoria_id):
         form = CategoriaForm(instance=categoria)
     return render(request, 'editar_categoria.html', {'form': form, 'categorias': categoria})
 
-# Excluir Categoria
 def excluir_categoria(request, categoria_id):
     categoria = get_object_or_404(Categoria, id=categoria_id)
     if request.method == 'POST':
         categoria.delete()
         return redirect('cadastro_categoria')
     return render(request, 'confirmar_exclusao_categoria.html', {'categorias': categoria})
-
-from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login, authenticate
 
 def register(request):
     if request.method == 'POST':
@@ -270,17 +220,9 @@ def register(request):
         form = UserCreationForm()
     return render(request, 'register.html', {'form': form})
 
-# gestao_cartoes/views.py
-from django.shortcuts import redirect
-from django.contrib.auth import logout
-
 def logout_view(request):
     logout(request)
     return redirect('login')
-
-# views.py
-
-from django.http import JsonResponse
 
 @login_required
 def gastos_do_cartao(request, cartao_id):
@@ -297,17 +239,12 @@ def gastos_do_cartao(request, cartao_id):
         if 1 <= mes_num <= 12:
             mes_atual = mes_num
 
-    # Definindo o próximo vencimento para o mês atual selecionado
     mes_vencimento = mes_atual if mes_atual >= date.today().month else mes_atual + 1
     ano_vencimento = ano_atual if mes_vencimento >= date.today().month else ano_atual + 1
     proximo_vencimento = date(ano_vencimento, mes_vencimento, cartao.vencimento)
-
-    # Ajusta a data de fechamento para ser 10 dias antes do próximo vencimento
     data_fim = proximo_vencimento - timedelta(days=10)
     data_inicio = data_fim - timedelta(days=30)
 
-    # Filtra os gastos do cartão usando o período de fechamento
     gastos = Gasto.objects.filter(cartao=cartao, data__gte=data_inicio, data__lt=data_fim)
-
     gastos_data = list(gastos.values('descricao', 'valor', 'data'))
     return JsonResponse({'gastos': gastos_data})
